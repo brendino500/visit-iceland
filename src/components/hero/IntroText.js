fdfcf31292bfcd22c2ebd03f785aa7e793210b27
brendino500 <brenda.ty@live.com>
2021-02-20 19:23:49 +0000

    intro text

--- src/components/hero/IntroText.js
+++ src/components/hero/IntroText.js
@@ -1,7 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  text: {
+    fontSize: 60,
+    textTransform: 'uppercase',
+    fontFamiy: 'Barlow',
+    letterSpacing: 3,
+    textAlign: 'right',
+  },
+}))
+
 export default function IntroText() {
+  const classes = useStyles()
   return (
     <div className="box-content">
-      <h3 className="w-5/6">
+      <h3 className={classes.text}>
         ICELAND IS A COUNTRY OF EXTREME GEOLOGICAL CONTRASTS. WIDELY KNOWN AS
         “THE LAND OF FIRE AND ICE” ICELAND IS HOME TO SOME OF THE LARGEST
         GLACIERS IN EUROPE, AND SOME OF THE WORLD’S MOST ACTIVE VOLCANOES.
