8a5213fe6a60aa122a59461de8471aa46df576ad
brendino500 <brenda.ty@live.com>
2021-02-28 17:49:27 +0000

    carousel content

--- src/components/carousel/CarouselMain.js
+++ src/components/carousel/CarouselMain.js
@@ -1,16 +1,29 @@
-import Carousel from '@brainhubeu/react-carousel'
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
+import { Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-
-import Slide from './CarouselSlide'
 
 const useStyles = makeStyles((theme) => ({
   container: {
     marginTop: 200,
-    height: '80vh',
+    height: '60vh',
   },
   image: {
     height: '20em',
+  },
+  slideLayout: {
+    display: 'flex',
+  },
+  textLayout: {
+    marginLeft: '5em',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Barlow',
+    fontSize: '1.6em',
+    textTransform: 'uppercase',
   },
 }))
 
@@ -34,25 +47,60 @@ export default function CarouselMain() {
 
   return (
     <div className={classes.container}>
-      <Carousel plugins={['arrows']}>
-        {/* <img
-          src={content[0].image}
-          alt={content[0].activity}
-          className={classes.image}
-        />
-        <img
-          src={content[1].image}
-          alt={content[1].activity}
-          className={classes.image}
-        />
-        <img
-          src={content[2].image}
-          alt={content[2].activity}
-          className={classes.image}
-        /> */}
+      <div>
+        <Typography className={classes.title}>Get Inspired</Typography>
+      </div>
+      <Carousel
+        plugins={[
+          'clickToChange',
+          'centered',
+          {
+            resolve: slidesToShowPlugin,
+            options: {
+              numberOfSlides: 2,
+            },
+          },
+        ]}
+      >
+        <div className={classes.slideLayout}>
+          <img
+            src={content[0].image}
+            alt={content[0].activity}
+            className={classes.image}
+          />
+          <div className={classes.textLayout}>
+            <Typography className={classes.text}>
+              {content[0].activity}
+            </Typography>
+          </div>
+        </div>
+        <div className={classes.slideLayout}>
+          <img
+            src={content[1].image}
+            alt={content[1].activity}
+            className={classes.image}
+          />
+          <div className={classes.textLayout}>
+            <Typography className={classes.text}>
+              {content[1].activity}
+            </Typography>
+          </div>
+        </div>
+        <div className={classes.slideLayout}>
+          <img
+            src={content[2].image}
+            alt={content[2].activity}
+            className={classes.image}
+          />
+          <div className={classes.textLayout}>
+            <Typography className={classes.text}>
+              {content[2].activity}
+            </Typography>
+          </div>
+        </div>
+        {/* <Slide content={content} />
         <Slide />
-        <Slide />
-        <Slide />
+        <Slide /> */}
       </Carousel>
     </div>
   )
--- src/components/carousel/CarouselSlide.js
+++ src/components/carousel/CarouselSlide.js
@@ -1,7 +1,8 @@
-export default function CarouselSlide() {
+export default function CarouselSlide({ content, index }) {
+  console.log('content', content)
   return (
     <div>
-      <h1>Slide</h1>
+      <h1>{content[0].activity}</h1>
     </div>
   )
 }
